f80cdf015a2e0d3649bce91cd662479512ceffa3
leoneckert <leoneckert@mailbox.org>
2022-04-14 13:14:53 +0800

    scraping 1 (setting up python)

---
+++ labs/lab9/scraping/scrape.py
@@ -0,0 +1,12 @@
+import requests
+print("works!")
+
+# 1. download a web page in plain text
+
+# 2. turn plain text into structure html object
+#       with the library called Beautiful Soup
+
+# 3. Traverse the html structure to find info we need
+#       and save all that info into a nicely structured object / dictionairy
+
+# 4. save the dictionairy as a data.json file. DONE
