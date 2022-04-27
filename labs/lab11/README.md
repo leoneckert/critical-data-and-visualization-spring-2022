82afe894bdb185c93bfa95a1d5d271a2d1f1123a
Leon Eckert <leoneckert@mailbox.org>
2022-04-27 23:20:31 +0800

    lab11

---
+++ labs/lab11/README.md
@@ -0,0 +1,18 @@
+## Lab 11: Scrolling & Force Graphs
+
+- show examples
+  - [http://www.bmdata.co.uk/titanic/](http://www.bmdata.co.uk/titanic/)
+  - [https://cdv.leoneckert.com/show/jerry/](https://cdv.leoneckert.com/show/jerry/)
+  - [in class 2021](https://leoneckert.github.io/critical-data-and-visualization-spring-2022/labs/lab11/in-class-2021)
+  - [advanced](https://leoneckert.github.io/critical-data-and-visualization-spring-2022/labs/lab11/graphMove-advanced)
+  - in class example
+- resource for scrolling
+  - [https://pudding.cool/process/scrollytelling-sticky/](https://pudding.cool/process/scrollytelling-sticky/)
+- Force Graph Video:
+  - [Exercise files](force-start.zip)
+  - [Video](https://drive.google.com/file/d/1vpud5i8zBfPOSu7tpyZPpWohZ3dB3bwo/view?usp=sharing) (39:55)
+- Scrolling Interaction and sticky Elements
+  - [boilerplate code](sticky-basic.zip)
+  - in class demo (if requested)
+    - [recording-2021](https://nyu.zoom.us/rec/share/lKmeSZY-GuW9Ic0pOmElHPDNI7S0aYXn9oqwauFBNfhYQtbNWhIo2xamLk8g_OM.NOKLHwRz5O5ibgbH)
+    - [in-class code 2021](in-class-2021)
---
+++ labs/lab11/graphMove-advanced/script.js
@@ -0,0 +1,259 @@
+let w = 1000;
+let h = 800;
+let wW = window.innerWidth;
+let wH = window.innerHeight;
+let padding = 90;
+let paddingTopBottom = (wH-h)/2;
+let paddingLeftRight = (wW-w)/2;
+
+
+let viz = d3.select("#vizContainer").append("svg")
+    .style("width", wW)
+    .style("height", wH*2)
+    .style("background-color", "lavender")
+;
+
+
+let bothGraphs = viz.append("g")
+    .attr("class", "bothGraphs")
+;
+let graphTranslationScale = d3.scaleLinear().domain([0, 1]).range([0, -wH])
+
+
+
+
+// bothGraphs.append("rect")
+//   .attr("x", paddingLeftRight)
+//   .attr("y", paddingTopBottom)
+//   .attr("width", w)
+//   .attr("height", h)
+//   .attr("fill", "grey")
+// ;
+// bothGraphs.append("rect")
+//   .attr("x", paddingLeftRight)
+//   .attr("y", wH+paddingTopBottom)
+//   .attr("width", w)
+//   .attr("height", h)
+//   .attr("fill", "grey")
+// ;
+
+// // get data
+d3.json("monarchs.json").then(function(incomingData){
+  data = formatData(incomingData);
+  // groupedByType = d3.group(data, d => d.type)
+  // console.log(groupedByType)
+  let types = data.map(d=>d.type).filter(onlyUnique); //see onlyUnique function at bottom
+  let typeCount = {}
+  types.forEach(d=>{
+    typeCount[d] = 0;
+  })
+  // console.log(typeCount)
+  let maxTypeCount = 0;
+  data.forEach(d=>{
+    // console.log(d, typeCount[d.tpe])
+    d.countInType = typeCount[d.type];
+    typeCount[d.type]++
+    maxTypeCount = Math.max(typeCount[d.type],maxTypeCount)
+
+
+  })
+
+  console.log(maxTypeCount)
+
+  console.log(data)
+
+
+  // build axis for both graphs
+  // graph1
+  // xscale and axis
+  let g1XScale = d3.scaleTime().range([paddingLeftRight, wW-paddingLeftRight]);
+  g1XScale.domain( d3.extent(data, d=>d.date) )
+  let g1XAxis = d3.axisBottom(g1XScale).tickFormat(d3.timeFormat("%-Y"));
+  let g1XAxisGroup = bothGraphs.append("g")
+      .attr("class", "g1xaxisgroup")
+      .attr("transform", "translate(0,"+(paddingTopBottom+h)+")")
+  ;
+  g1XAxisGroup.call(g1XAxis);
+  //yscale and axis
+  let g1YScale = d3.scaleBand().range([paddingTopBottom, wH-paddingTopBottom]);
+  g1YScale.domain(types);
+  let g1YAxis = d3.axisLeft(g1YScale);
+  let g1YAxisGroup = bothGraphs.append("g")
+      .attr("class", "g1yaxisgroup")
+      .attr("transform", "translate("+paddingLeftRight+",0)")
+
+  ;
+  g1YAxisGroup.call(g1YAxis);
+  g1YAxisGroup.selectAll("line").attr("display", "none");
+  g1YAxisGroup.selectAll("path").attr("display", "none");
+  g1YAxisGroup.selectAll("text")
+      .attr("text-anchor", "middle")
+      .attr("transform", "rotate(-90)")
+      .attr("x", "0")
+  ;
+
+  // build axis for both graphs
+  // graph2
+  // xscale and axis
+  let g2XScale = d3.scaleBand().range([paddingLeftRight, wW-paddingLeftRight]);
+  g2XScale.domain(types)
+  let g2XAxis = d3.axisBottom(g2XScale);
+  let g2XAxisGroup = bothGraphs.append("g")
+      .attr("class", "g2xaxisgroup")
+      .attr("transform", "translate(0,"+(wH+paddingTopBottom+h)+")")
+  ;
+  g2XAxisGroup.call(g2XAxis);
+  //yscale and axis
+  // let maxType = d3.max(groupedByType, d=>{
+  //   return d[1].length;
+  // })
+  // console.log(maxType)
+  let g2YScale = d3.scaleLinear().range([wH+paddingTopBottom, wH+wH-paddingTopBottom]);
+  g2YScale.domain([maxTypeCount, 0])
+  // let g2YAxis = d3.axisLeft(g2YScale);
+  // let g2YAxisGroup = bothGraphs.append("g")
+  //     .attr("class", "g2yaxisgroup")
+  //     .attr("transform", "translate("+paddingLeftRight+",0)")
+
+  // ;
+  // g2YAxisGroup.call(g2YAxis);
+
+  
+
+  let graphics = bothGraphs.append("g").attr("class", "graphics");
+
+
+  // build graph 1
+  function showGraph1(){
+    let datagroups = graphics.selectAll(".datagroup").data(data, d=>d.event);
+    let enterSelection = datagroups.enter()
+      .append("g")
+        .attr("class", "datagroup")
+        .attr("transform", function(d){
+          // console.log(d, "translate("+ g1XScale(d.date) +","+ (g1YScale(d.type) + g1YScale.bandwidth()/2 )+")");
+          return "translate("+ g1XScale(d.date) +","+ (g1YScale(d.type) + g1YScale.bandwidth()/2 )+")"
+        })
+    ;
+    enterSelection.append("circle")
+      .attr("r", 10)
+      .attr("opacity", 0.5)
+      .attr("fill", "red")
+    ;
+
+    //update:
+    datagroups
+    .transition()
+    .duration(1000)
+    .attr("transform", function(d){
+      // console.log(d, "translate("+ g1XScale(d.date) +","+ (g1YScale(d.type) + g1YScale.bandwidth()/2 )+")");
+      return "translate("+ g1XScale(d.date) +","+ (g1YScale(d.type) + g1YScale.bandwidth()/2 )+")"
+    })
+  }
+
+  showGraph1();
+
+
+  function showGraph2(){
+    let datagroups = graphics.selectAll(".datagroup").data(data,  d=>d.event);
+    let enterSelection = datagroups.enter()
+      .append("g")
+        .attr("class", "datagroup")
+        .attr("transform", function(d, i){
+          // console.log(d, "translate("+ g1XScale(d.date) +","+ (g1YScale(d.type) + g1YScale.bandwidth()/2 )+")");
+          return "translate("+ (g2XScale(d.type)+g2XScale.bandwidth()/2) +","+ (g2YScale(d.countInType) )+")"
+        })
+    ;
+    enterSelection.append("circle")
+      .attr("r", 10)
+      .attr("opacity", 0.5)
+      .attr("fill", "red")
+    ;
+
+    //update elements:
+    datagroups
+    .transition()
+    .duration(1000)
+    .attr("transform", function(d, i){
+      // console.log(d, "translate("+ g1XScale(d.date) +","+ (g1YScale(d.type) + g1YScale.bandwidth()/2 )+")");
+      return "translate("+ (g2XScale(d.type)+g2XScale.bandwidth()/2) +","+ (g2YScale(d.countInType) )+")"
+    })
+
+  }
+
+  // showGraph2();
+
+
+  
+
+  enterView({
+    selector: '.secondWindow',
+    enter: function(el) {
+      console.log('a special element entered');
+      // console.log("Color")
+      // myData[0][3] = "lightyellow"
+      // updateGraph();
+  
+    },
+    exit: function(el) {
+      console.log('a special element exited');
+      // myData[0][3] = "lightblue"
+      // updateGraph();
+      showGraph1()
+  
+    },
+    prev: undefined,
+    progress: function(el, progress) {
+      console.log("the special element's progress is:", progress, this.prev);
+
+
+      bothGraphs.attr("transform", ()=>{
+        return "translate(0, "+graphTranslationScale(progress)+")"
+      });
+      console.log(graphTranslationScale(progress))
+// let graphTranslationScale = d3.scaleLinear().domain([0, 100]).range([0, -wH])
+
+      if(this.prev < progress && progress > 0.5){
+        showGraph2()
+      }else if(this.prev > progress && progress <= 0.5){
+        showGraph1()
+      }
+
+
+
+      // if(progress==1){
+      //   showGraph2()
+      // }
+      this.prev = progress;
+    },
+    offset: 0.5, // enter at middle of viewport
+    // once: true, // trigger just once
+  });
+
+
+
+  
+
+
+
+
+
+
+
+});
+
+
+let timeParse = d3.timeParse("%Y");
+
+function formatData(incoming){
+  let keys = Object.keys(incoming.Dates);
+  return keys.map((d)=>{
+    incoming.Dates[d].date = timeParse(incoming.Dates[d].date)
+    return incoming.Dates[d];
+  });
+
+}
+
+//from: https://stackoverflow.com/a/14438954
+function onlyUnique(value, index, self) {
+    return self.indexOf(value) === index;
+}
